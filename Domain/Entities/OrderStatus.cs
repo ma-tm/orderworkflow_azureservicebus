f21b819fe654f233f1c5f96b30c4f0ec678d42b6
Muhammad Asif <muhammad.asif@thriftmetrics.com>
2026-01-03 12:30:05 +0500

    Solution initialized. Domain entities are being created

---
+++ Domain/Entities/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Entities
+{
+    public enum OrderStatus
+    {
+        Placed,
+        PendingApproval,
+        Rejected,
+        Accepted,
+        InPreparation,
+        ReadyForPickup,
+        AssignedToRider,
+        PickedUp,
+        OutForDelivery,
+        Delivered,
+        Cancelled
+    }
+}
